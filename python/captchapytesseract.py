4401ac72af51cc634fa9d0951e8cc8bf3df69dc8
Maxwim <55312190+Maxwim@users.noreply.github.com>
2022-03-13 10:47:14 +0100

    Create captchapytesseract.py

---
+++ python/captchapytesseract.py
@@ -0,0 +1,29 @@
+#Challenge root me, le script ne réussis pas à 100%, Pytesseract à un peu de mal a bien lire les captcha
+from os import path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from base64 import b64decode
+from PIL import Image
+from io import BytesIO
+from re import sub
+import pytesseract
+def run():
+    url = "http://challenge01.root-me.org/programmation/chXX/"
+    driver = webdriver.Firefox(executable_path="driver/geckodriver")
+    driver.get(url)
+
+    images = driver.find_elements_by_tag_name('img')
+    img = ''
+    for image in images:
+        img = image.get_attribute('src')
+    imgs = img.replace('data:image/png;base64,', '')
+    im = Image.open(BytesIO(b64decode(imgs)))
+    im.save('image.png', 'PNG')
+    keys = pytesseract.image_to_string(Image.open('image.png'))
+    key = sub(r"[^a-zA-Z0-9]","",keys)
+    print(key)
+
+    driver.find_element(By.XPATH, "//html/body/form//input").send_keys(key)  
+    btn = driver.find_element(By.XPATH, "//input[@value='Try']") 
+    btn.click()
+run()
